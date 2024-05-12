fdf27cf8072fae401fe06eec6f56d9799ddc291c
Will Hodges <hodges.wt@gmail.com>
2024-05-12 15:51:45 -0400

    Add submission handling to guess box

--- lib/widgets/guess_box.dart
+++ lib/widgets/guess_box.dart
@@ -3,6 +3,7 @@ import 'package:cinemadle/data_model/paginated_results.dart';
 import 'package:cinemadle/movie_data.dart';
 import 'package:cinemadle/resource_manager.dart';
 import 'package:cinemadle/resources.dart';
+import 'package:cinemadle/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 
@@ -26,39 +27,56 @@ class _GuessBoxState extends State<GuessBox> {
   SuggestionsController<String> suggestionsController =
       SuggestionsController<String>();
 
+  TextField? _guessBoxField;
+
+  _submit(String data) {
+    widget.inputCallback(data);
+    _guessBoxField?.controller?.clear();
+  }
+
+  TextField _getGuessBoxField(
+      TextEditingController? controller, FocusNode? focusNode) {
+    _guessBoxField = TextField(
+      controller: controller,
+      focusNode: focusNode,
+      decoration: InputDecoration(
+        labelText: rm.getResource(Resources.inputBoxHintText),
+      ),
+      onSubmitted: (data) {
+        _submit(data);
+      },
+      onChanged: (value) async {
+        setState(() {
+          autofillHints = [];
+        });
+        if (value.length > 3) {
+          suggestionsController.isLoading = true;
+          PaginatedResults search = await md.searchMovie(value);
+
+          List<String> newTitles = [];
+          for (MovieRecord movie in search.results.take(4)) {
+            newTitles.add(movie.title);
+          }
+
+          setState(() {
+            autofillHints = newTitles;
+          });
+
+          suggestionsController.isLoading = false;
+        }
+      },
+    );
+
+    return _guessBoxField!;
+  }
+
   @override
   Widget build(BuildContext context) {
     return TypeAheadField<String>(
       suggestionsCallback: (search) => autofillHints,
       suggestionsController: suggestionsController,
       builder: (context, controller, focusNode) {
-        return TextField(
-          controller: controller,
-          focusNode: focusNode,
-          decoration: InputDecoration(
-            labelText: rm.getResource(Resources.inputBoxHintText),
-          ),
-          onChanged: (value) async {
-            setState(() {
-              autofillHints = [];
-            });
-            if (value.length > 3) {
-              suggestionsController.isLoading = true;
-              PaginatedResults search = await md.searchMovie(value);
-
-              List<String> newTitles = [];
-              for (MovieRecord movie in search.results.take(4)) {
-                newTitles.add(movie.title);
-              }
-
-              setState(() {
-                autofillHints = newTitles;
-              });
-
-              suggestionsController.isLoading = false;
-            }
-          },
-        );
+        return _getGuessBoxField(controller, focusNode);
       },
       loadingBuilder: (context) =>
           ListTile(title: Text(rm.getResource(Resources.loading))),
@@ -67,7 +85,9 @@ class _GuessBoxState extends State<GuessBox> {
           title: Text(data),
         );
       },
-      onSelected: (city) {},
+      onSelected: (data) {
+        _submit(data);
+      },
       hideOnEmpty: true,
     );
   }
